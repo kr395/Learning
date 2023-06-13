07ce698a3642dd8b391128fb09b20279e8442c44
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-06-13 13:40:24 +0530

    First Commit

---
+++ Python/Learn.py
@@ -0,0 +1,76 @@
+
+''' Hello It's my Document and note on Learning Python
+#Print
+print("Hello World")
+# Comments in python
+#Multiline ''' '''
+# Variables
+name = 'Gopal Kumar Das'
+print(name)
+# Data Types
+string = 'Gopal'
+number = 56
+float = 53.05
+Boolean = True # Or False
+None_Value = None
+print(float)
+number = 56
+f = 5236.6
+string = 'Fifty Six -'
+print(string + str(number))
+print(f + number)
+# Operators
+# Arithmetic
+num_1 = 10
+num_2 = 5
+print('num_1 + num_2 is -', num_1 + num_2)
+print('num_1 - num_2 is -', num_1 - num_2)
+print('num_1 * num_2 is -', num_1 * num_2)
+print('num_1 / num_2 is -', num_1 / num_2)
+print('num_1 // num_2 is -', num_1 // num_2)
+print('num_1 ** num_2 is -', num_1 ** num_2)
+print('num_1 % num_2 is -', num_1 % num_2)
+
+# Assignment Operators
+a = 6
+a /= 2
+print(str(a))
+a *= 2
+print(str(a))
+a += 2
+print(str(a))
+a //= 2
+print(str(a))
+a %= 2
+print(str(a))
+
+# Comparison Operators
+x = 5
+y = 3
+z = 5
+print(x > y)
+print(x < y)
+print(x != y)
+print(x == z)
+
+# Logical Operator
+x = 5
+y = 3
+z = 5
+print(x == z and x < y)
+print(x == z or x < y)
+print(not(True))
+
+'''
+# Strings and Strings Method
+name = 'Gopal Kumar Das'
+print(name[0:9])
+
+
+
+
+
+
+
+
+
