be16e05f6e6c2b347d97b20a606dc0106b62c253
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-06-19 18:22:57 +0530

    JS Learn

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+
+
+
+let count = 0
+function incr() {
+    count += 1
+    document.getElementById("count").innerHTML = count
+}
+function dicr() {
+    count -= 1
+    document.getElementById("count").innerHTML = count
+}
+
