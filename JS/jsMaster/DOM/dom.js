01fb2d72e5cb18fbfd325c43d0d83a9f0fe5971c
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-08-04 18:57:55 +0530

    Learnt DOM

---
+++ JS/jsMaster/DOM/dom.js
@@ -0,0 +1,13 @@
+const body = document.body
+// Append and append child 
+// body.append("Gopal", "Bye")
+
+// const div = document.createElement('div')
+// body.append(div) 
+
+let x = 3.2655646;
+
+// x = Math.round(x)
+
+
+console.log(x)
