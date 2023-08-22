1ff25bf8c18a798082e72a49c2705e22b41f6c26
Gopal Kumar Das <krgopalkumar706@gmail.com>
2023-08-22 15:41:32 +0530

    appandChild,remove,adjasen Itme Etc added

--- JS/jsMaster/DOM/dom.js
+++ JS/jsMaster/DOM/dom.js
@@ -1,1 +1,112 @@
-const heading = document.getElementById("head")
+// 🔥 document.getElementById
+// document.querySelector()
+// const head = document.querySelector('#head')
+// // Inner Html and textContent
+// console.log(head.textContent)
+
+// const div = document.querySelector('.container')
+// div.style.transform = 'rotate(0deg)'
+// console.log(div)
+
+//🔥 get and set attribute
+// const aTag = document.querySelector('a');
+// aTag.style.all = 'unset';
+// const atr = aTag.getAttribute('href')
+// aTag.setAttribute('href','https://www.google.com/')
+
+// const allLi = document.querySelectorAll('.list') // Giving Node List
+// const tagList = document.getElementsByClassName('list') // Giving HTML Collection
+// // This is array like object
+// console.log(tagList)
+// console.log(allLi)
+// // let listArray = Array.from(tagList)
+// allLi.forEach((item, index)=>{  // We can itrate over node list easyly using for each
+//       item.style.color = 'red'
+// })
+
+// 🔥  We can't run for each loop in HTML Collection
+
+// for (const li of tagList) {
+//     li.addEventListener('click',()=>{
+//         console.log("Gopal")
+//     })
+// }
+
+//  🔥 Inner HTML and textContent
+
+// const ul = document.querySelector('ul')
+
+// ul.innerHTML = '<button id=\'newBtn\'>Click</button>'
+
+// const createdBtn = document.querySelector('#newBtn')
+// createdBtn.addEventListener('click',()=>{
+//     console.log("Gopal")
+// })
+
+// const div =  document.querySelector('.container')
+// const rootNode = document.getRootNode()
+// const htmlElementNode = rootNode.childNodes[1]
+
+// const head = document.getElementById('head');
+// // console.log(head.nextElementSibling)
+// // console.log(head.nextSibling)
+// //  ChildNode,Parent Elements,
+
+// const div = head.parentElement;
+// const body = div.parentNode;
+// const childrens = div.children
+// for (const children of childrens) {
+//     children.style.color = 'red';
+// }
+// // console.log(children)
+// console.log(body)
+// const heading = div.querySelector('#head')
+// console.log(heading)
+
+
+// 🔥 ClassList
+
+// const Lis = document.querySelectorAll('li');
+// console.log(Lis);
+
+// for (const li of Lis) {
+//     li.classList.add('newClass')
+//     li.classList.toggle('newClass')
+   
+// }
+
+// console.log(Lis);
+
+
+//  🔥 Small Todo task
+
+// createElement, append,prepend,remove,before,after
+
+// const addBtn = document.querySelector('.container button')
+// const ul = document.querySelector('.container ul')
+// const taskInput = document.querySelector('input')
+// ul.classList.add('style')
+// addBtn.addEventListener('click',()=>{
+//     //  const list = document.createElement('li')
+//     //  list.textContent = 'Gopal'
+//     //  const btn = document.createElement('button')
+//     //  btn.textContent = 'X'
+//     //  ul.append(list)
+//     //  ul.append(btn)
+//     // const createdBtn = document.querySelector('ul button')
+//     // createdBtn.addEventListener('click',()=>{
+//     //     ul.remove('li')
+//     // })
+//     // const taskName = taskInput.value
+//     // const html = `<li>${taskName}<button>X</button></li>`
+//     // ul.insertAdjacentHTML('beforeend',html)
+//     // taskInput.value = ''
+//     // const createdBtns = document.querySelectorAll('ul li button')
+//     // createdBtns.addEventListener('click',(e)=>{
+//     //       console.log(createdBtns)
+//     // })
+  
+// })
+
+//  2nd Video 6.00.00
+
